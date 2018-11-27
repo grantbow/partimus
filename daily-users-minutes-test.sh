f069299637c91cffbda4fb8c0e9f0fe7c29621e1
Grant Bowman <grantbow@gmail.com>
2018-11-27 10:11:20 -0800

    daily-users-minutes processed well

--- daily-users-minutes-test.sh
+++ daily-users-minutes-test.sh
@@ -3,8 +3,22 @@ t1=$(date -d "-2 days" | cut -d\  -f1-3)
 t2=$(date -d "-1 days" | cut -d\  -f1-3)
 t3=$(date -d "today" | cut -d\  -f1-3)
 
+sum=0
+
 printf "%s\n%s\n%s\n" \
 "userid    pts/0        10.0.0.1   $t1 03:47 - 09:44  (05:56)" \
 "userid    pts/0        10.0.0.1   $t2 03:47 - 09:44  (05:56)" \
+"userid1    pts/1        10.0.0.1   $t2 05:47 - 11:44  (05:56)" \
+"userid1    pts/2        10.0.0.1   $t2 07:47 - 13:44  (05:56)" \
+"userid2    pts/2        10.0.0.1   $t2 11:47 - 17:44  (05:56)" \
 "userid    pts/0        10.0.0.1   $t3 03:47 - 09:44  (05:56)" | ./daily-users-minutes.sh
+
+# process lines twice to get output reports:
+
+#userid login count
+# | cut -d\  -f1 | uniq -c
+
+#total minutes, added by awk, bc not needed
+# | cut -d\  -f2 | awk '{s+=$0} END {print s}'
+
 
--- daily-users-minutes.sh
+++ daily-users-minutes.sh
@@ -1,11 +1,37 @@
 #!/bin/sh
-# assumes date output format and last command for info
+# sample STDIN input:
+#     userid    pts/0        10.0.0.1   Thu Nov  1 03:47 - 09:44  (05:56)
 target=$(date -d "yesterday" | cut -d\  -f2-3)
 # i.e. Nov 1
+# assumes date output format
 while read line
 do
-    # sample input: userid    pts/0        10.0.0.1   Thu Nov  1 03:47 - 09:44  (05:56)
-    date=$(date -d "$(echo $line | cut -d\  -f5-8)" +%s)
-    if [ $date -ge "$(date -d "$target 00:00" +%s)" ] && [ $date -le "$(date -d "$target 23:59" +%s)" ] ; then  echo $line; fi
+    date=$(date -d "$(echo $line | cut -d\  -f5-8)" +%s) # each line date
+    if [ $date -ge "$(date -d "$target 00:00" +%s)" ] && [ $date -le "$(date -d "$target 23:59" +%s)" ]
+    then
+        #echo $line # each login for yesterday
+        user=$(echo $line | cut -d\  -f1)
+        time=$(echo $line | cut -d\  -f1,10 | uniq -c | cut -d\  -f9 | tr -d \(\))
+        #t="05:56"
+        minutes=$(echo $(echo $time | cut -d: -f1 | sed s/\$/\*60/g | bc) + $(echo $time | cut -d: -f2) | bc)
+        #m="356"
+        echo "$user $minutes"
+        ## sample output each login: userid 356
+    fi
 done
+
+# 
+# sample input: userid    pts/0        10.0.0.1   Thu Nov  1 03:47 - 09:44  (05:56)
+## | cut -d\  -f1,10 | uniq -c | cut -d\  -f9 | tr -d \(\)
+## outputs 05:56
+#
+#t="05:56"
+## hours*60 - sed replaces end of line with stdin for each line
+## | cut -d: -f1 | sed s/\$/\*60/g | bc
+## minutes
+## | cut -d: -f2
+## then echo them to add
+#echo $(echo $t | cut -d: -f1 | sed s/\$/\*60/g | bc) + $(echo $t | cut -d: -f2) | bc
+
+
 
